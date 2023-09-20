b70fa6bf94031bee6d0d3b3f983442f288d89936
mncbl <mateusncbl@gmail.com>
2023-09-20 17:06:02 -0300

    coloquei as funçoes de escolhe numero de produto

--- codigo/main.go
+++ codigo/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"fmt"
 	c "Ap1/codigo/clientes"
 	p "Ap1/codigo/produtos"
+	"fmt"
 )
 
 func main() {
-	c.Cli()
-	fmt.Println("--------------")
-	fmt.Println(p.TotalProdutos)
-	
+	for {
+		var op int
+
+		op = 0
+		fmt.Println("escolha a função que deseja")
+		fmt.Println("1- voltando para a interface")
+		fmt.Println("2- Add produtos Até 10 produtos ")
+		fmt.Scanln(&op)
+		switch op {
+		case 1:
+			{
+				{
+					fmt.Println("VOLTANDO PRA INTERFACE")
+				}
+			}
+
+		case 2:
+			{
+				c.Cli()
+				fmt.Println("ADD Produto")
+				fmt.Println(p.TotalProdutos)
+
+			}
+		}
+
+	}
 }
