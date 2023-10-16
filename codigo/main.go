f9d7d36f4b2ac572fe5778b46ba2332169b65067
brunopdrocha <brunopdrocha@gmail.com>
2023-10-16 11:57:31 -0300

    Quase completo CASO 5/6/7

--- codigo/main.go
+++ codigo/main.go
@@ -1,36 +1,58 @@
+// main.go
+
 package main
 
 import (
-	c "Ap1/codigo/clientes"
-	p "Ap1/codigo/produtos"
+	"Ap1/codigo/produtos"
 	"fmt"
 )
 
 func main() {
+	var opcao int
+
+	fmt.Println("Bem-vindo à CLI do Sistema de Pedidos!")
+
 	for {
-		var op int
+		fmt.Println("Menu:")
+		fmt.Println("1 - Cadastrar Produto")
+		fmt.Println("2 - Remover Produto")
+		fmt.Println("3 - Exibir Todos os Produtos")
+		fmt.Println("4 - Buscar Produto por ID")
+		fmt.Println("5 - Adicionar Produto ao Pedido")
+		fmt.Println("6 - Expedir Pedido")
+		fmt.Println("7 - Exibir Métricas do Sistema")
+		fmt.Println("0 - Sair")
+		fmt.Print("Escolha uma opção: ")
+		fmt.Scanln(&opcao)
 
-		op = 0
-		fmt.Println("escolha a função que deseja")
-		fmt.Println("1- voltando para a interface")
-		fmt.Println("2- Add produtos Até 10 produtos ")
-		fmt.Scanln(&op)
-		switch op {
+		switch opcao {
 		case 1:
-			{
-				{
-					fmt.Println("VOLTANDO PRA INTERFACE")
-				}
-			}
-
+			produto := produtos.Produto{}
+			produto.CadastrarProduto()
 		case 2:
-			{
-				c.Cli()
-				fmt.Println("ADD Produto")
-				fmt.Println(p.TotalProdutos)
+			produtos.RemoverProduto()
+			fmt.Scanln(&opcao)
+		case 3:
+			produtos.ExibirProdutos()
+		case 4:
+			produto := produtos.Produto{}
+			produto.BuscaProduto()
+		case 5:
+			pedido := produtos.Pedido{}
+			pedido.AdicionaPedido()
+			fmt.Scanln(&opcao)
 
-			}
+		case 6:
+			produtos.ExpedirPedido()
+
+		case 7:
+			produtos.ExibirMetricasSistema()
+
+		case 0:
+			fmt.Println("Saindo...")
+			return
+		default:
+			fmt.Println("Opção inválida. Tente novamente.")
 		}
-
 	}
 }
